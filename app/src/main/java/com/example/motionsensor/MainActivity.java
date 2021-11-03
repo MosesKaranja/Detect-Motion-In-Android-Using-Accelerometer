eb7776602011341244019e56551a20563d3e936b
MosesKaranja <moseskaranja830@gmail.com>
2021-11-03 22:30:32 +0300

    Detect Acceleration using accelerometer

--- app/src/main/java/com/example/motionsensor/MainActivity.java
+++ app/src/main/java/com/example/motionsensor/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.motionsensor;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,79 +8,124 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    SensorManager sensorManager = null;
-    TextView textView1 = null;
-    Button btnOtherActivity = null;
-    List list;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    SensorEventListener sensorEventListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            float[] values = event.values;
-            textView1.setText("x: "+values[0]+"\ny: "+values[1]+"\nz: "+values[2]);
+    private TextView textViewShowStuff;
+    private SensorManager sensorMan;
+    private Sensor accelerometer;
 
+    private float[] mGravity;
+    private double mAccel;
+    private double mAccelCurrent;
+    private double mAccelLast;
 
-
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.N)
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            Log.i("sAccuracy", String.valueOf(accuracy));
-            Log.i("sName",sensor.getName());
-            Log.i("sVendor",sensor.getVendor());
-            Log.i("sPower", String.valueOf(sensor.getPower()));
-            Log.i("sId",String.valueOf(sensor.getId()));
-            Log.i("sMaxRange", String.valueOf(sensor.getMaximumRange()));
-
-        }
-    };
+    //private LogFragment mLogFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewShowStuff = findViewById(R.id.textViewShowStuff);
+        sensorMan = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        textView1 = findViewById(R.id.textViewData);
-        btnOtherActivity = findViewById(R.id.buttonOtherActivity);
-
-        list = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
-
-        if (list.size() > 0){
-            sensorManager.registerListener(sensorEventListener,(Sensor) list.get(0), SensorManager.SENSOR_DELAY_NORMAL);
-
+        Log.i("mAccel", String.valueOf(mAccel));
+        Log.i("mAccelCurrent", String.valueOf(mAccelCurrent));
+        Log.i("mAccelLast", String.valueOf(mAccelLast));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Log.i("accMaxDelay", String.valueOf(accelerometer.getMaxDelay()));
         }
-        else{
-            Toast.makeText(getApplicationContext(), "Error: No Accelerometer", Toast.LENGTH_LONG).show();
-        }
+        Log.i("accMinDelay", String.valueOf(accelerometer.getMinDelay()));
 
-        btnOtherActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), other_activity.class);
-                startActivity(intent);
 
-            }
-        });
+        // Initialize the logging framework.
+        //initializeLogging();
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    public void onResume() {
+        super.onResume();
+//        sensorMan.registerListener(this, accelerometer,
+//                SensorManager.SENSOR_DELAY_UI);
 
-        if (list.size() > 0){
-            sensorManager.unregisterListener(sensorEventListener);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //This code below is aimed at decreasing the speed at which we get the sensor data to 1 second.
+            //That sensor data is coming in at a crazily fast rate, am think this fast rate is likely to eat up the battery life.
+            sensorMan.registerListener(this, accelerometer, 400000,400000);
+        }
+        else{
+            //In the code below we simply get sensor data with its default input.
+            sensorMan.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        }
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorMan.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            mGravity = event.values.clone();
+            // Shake detection
+            double x = mGravity[0];
+            double y = mGravity[1];
+            double z = mGravity[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = Math.sqrt(x * x + y * y + z * z);
+            double delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            // Make this higher or lower according to how much
+            // motion you want to detect
+            String showStuff = "X value: "+x+"\n "+"Y value: "+y+"\n"+"Z value:"+z+"\n"+"Last Acc: "+mAccelLast+"\n"+"Curr Acc:"+mAccelCurrent+"\n"+"Ur Acc"+mAccel+"\n";
+
+            textViewShowStuff.setText(showStuff);
+
+            Log.i("mAccelInSensor", String.valueOf(mAccel));
+            Log.i("mAccelCurrentInSensor", String.valueOf(mAccelCurrent));
+            Log.i("mAccelLastInSensor", String.valueOf(mAccelLast));
+            Log.i("----------------------","----------------------");
+            Log.i("----------------------","----------------------");
+            Log.i("----------------------","----------------------");
+            Log.i("----------------------","----------------------");
+            Log.i("----------------------","----------------------");
+            Log.i("----------------------","----------------------");
+
+            //The 1.0 below was 1.5
+            if(Math.abs(mAccel) > 1.0){
+                //Log.i(MainActivity.class.getName(), String.valueOf(mAccel));
+                Log.i("Motion_Detected", String.valueOf(mAccel));
+                // do something
+                //Toast.makeText(getApplicationContext(), "MOvement Detected", Toast.LENGTH_SHORT).show();
+                textViewShowStuff.setTextColor(Color.RED);
+            }
+            else{
+                textViewShowStuff.setTextColor(Color.BLACK);
+
+            }
         }
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // required method
+    }
+
+
+
 }
+
